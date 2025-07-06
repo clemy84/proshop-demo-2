6a342dffa4f1a57a91f62eab13af15ec14c08bb7
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-07-06 21:28:54 +0100

    feat: add responsive layout with Bootstrap and implement header, footer, and product components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -7,23 +8,17 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md">
         <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              alt="ProShop Logo"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="ProShop Logo" />
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
+              <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href="/login">
+              <Nav.Link as={Link} to="/login">
                 <FaUser /> Login
               </Nav.Link>
             </Nav>
