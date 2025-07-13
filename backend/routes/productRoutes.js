ce26fb12b254d85316e4c7b5813399b8c355195d
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-07-13 10:51:49 +0100

    commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
+
+router.route('/').get(getProducts); // Fetch all products
+router.route('/:id').get(getProductById); // Fetch a product by ID
+
+export default router;
