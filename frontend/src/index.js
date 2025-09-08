84ea29b2cbab1f22b90b0feb2d59b79d8debe47d
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-09-08 21:30:58 +0100

    comit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import LoginSreen from './screens/LoginSreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
       </Route>
     </Route>
   )
