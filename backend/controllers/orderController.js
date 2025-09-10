a6bc6cccc8a1de7ff2109998d0f97181176c6467
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-09-10 22:57:39 +0100

    commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -68,21 +68,52 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route PUT /api/orders/:id/pay
 // @access Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('Update order to paid');
+  const oreder = await Order.findById(req.params.id);
+
+  if (oreder) {
+    oreder.isPaid = true;
+    oreder.paidAt = Date.now();
+    oreder.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await oreder.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc update order to delivered
 // @route PUT /api/orders/:id/deliver
 // @access Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('Update order to delivered');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc get all orders
 // @route GET /api/orders
 // @access Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.send('Get all orders');
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.status(200).json(orders);
 });
 
 export {
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProducts); // Fetch all products
-router.route('/:id').get(getProductById); // Fetch a product by ID
+router.route('/').get(getProducts).post(protect, admin, createProduct); // Fetch all products
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct); // Fetch a product by ID
 
 export default router;
