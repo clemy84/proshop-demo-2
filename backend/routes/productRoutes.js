f99c83a68b4733d3aaf52aacc4c751d1c08ee473
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-09-14 22:49:11 +0100

    commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -15,5 +16,6 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct); // Fetch a product by ID
+router.route('/:id/reviews').post(protect, createProductReview); // Create a new review for a product
 
 export default router;
