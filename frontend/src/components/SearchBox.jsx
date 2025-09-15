3fc94066339d1bc141d8efc8d55edc0a3ac624ca
stephen cleminshaw <stephencleminshaw@yahoo.co.uk>
2025-09-15 14:57:58 +0100

    commit

---
+++ frontend/src/components/SearchBox.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+import { set } from 'mongoose';
+
+const SearchBox = () => {
+  const navigate = useNavigate();
+  const { keyword: urlkeyword } = useParams();
+  const [keyword, setKeyword] = useState(urlkeyword || '');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      setKeyword('');
+      navigate(`/search/${keyword}`);
+    } else {
+      navigate('/');
+    }
+  };
+
+  return (
+    <Form onSubmit={submitHandler} className="d-flex">
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder="Search Products..."
+        className="mr-sm-2 ml-sm-5"
+        value={keyword}
+      ></Form.Control>
+      <Button type="submit" variant="outline-light" className="p-2 mx-2">
+        <FaSearch />
+      </Button>
+    </Form>
+  );
+};
+
+export default SearchBox;
